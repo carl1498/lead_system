90b72589a0af656f6b3c1ab758eace61d5cd80c8
Benedict Badilles <leadtbs.it@gmail.com>
2019-05-20 20:46:52 +0800

    employee view profile

--- resources/views/pages/employees.blade.php
+++ resources/views/pages/employees.blade.php
@@ -29,8 +29,8 @@
             <!-- Profile Image -->
             <div class="box box-primary">
                 <div class="box-body box-profile">
-                    <div style="width: auto; height: 125px;">
-                        <img id="p_picture" class="profile-user-img img-responsive img-rounded pull-left" src="./img/avatar5.png" alt="User Profile Picture" style="max-width: 97px; max-height: 97px;">
+                    <div style="width: auto; height: 200px;">
+                        <img id="p_picture" class="profile-user-img img-responsive img-rounded" src="./img/avatar5.png" alt="User Profile Picture" style="max-width: 97px; max-height: 97px;">
                         
                         <h4 id="p_emp_name" class="profile-username text-center">Employee Name</h4>
 
