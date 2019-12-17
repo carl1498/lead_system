34ccf72b9ddb020738ce49cbe6c74af9e0d28d75
Benedict Badilles <leadtbs.it@gmail.com>
2019-12-17 09:01:25 +0800

    marketing can access client

--- app/Http/Middleware/Client.php
+++ app/Http/Middleware/Client.php
@@ -18,12 +18,13 @@ class Client
     public function handle($request, Closure $next)
     {
         $id = Auth::user()->id;
-        $user = User::with('employee.role')->find($id);
+        $user = User::with('employee.role', 'employee.branch')->find($id);
         $authorized = ['President', 'Finance Director', 'HR/Finance Head', 'IT Officer', 
                         'Assistant Finance Officer'];
 
         foreach($authorized as $auth){
-            if($user->employee->role->name == $auth && $user->employee->employment_status != 'Resigned'){
+            if($user->employee->role->name == $auth && $user->employee->employment_status != 'Resigned'
+                || ($user->employee->role->name == 'Marketing Officer' && $user->employee->branch->name == 'Makati')){
                 return $next($request);
             }
         }
